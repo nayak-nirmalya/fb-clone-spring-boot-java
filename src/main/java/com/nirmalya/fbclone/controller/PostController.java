f35a46381254327cbf817543f7e49cf044a1b454
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-03 09:17:50 +0530

    defined constructors for service and repo

---
+++ src/main/java/com/nirmalya/fbclone/controller/PostController.java
@@ -0,0 +1,18 @@
+package com.nirmalya.fbclone.controller;
+
+import com.nirmalya.fbclone.service.PostService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(value = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1/posts")
+public class PostController {
+
+    private PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+}
--- src/main/java/com/nirmalya/fbclone/service/PostServiceImpl.java
+++ src/main/java/com/nirmalya/fbclone/service/PostServiceImpl.java
@@ -1,7 +1,14 @@
 package com.nirmalya.fbclone.service;
 
+import com.nirmalya.fbclone.repository.PostEntityRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PostServiceImpl implements PostService {
+
+    private PostEntityRepository postEntityRepository;
+
+    public PostServiceImpl(PostEntityRepository postEntityRepository) {
+        this.postEntityRepository = postEntityRepository;
+    }
 }
