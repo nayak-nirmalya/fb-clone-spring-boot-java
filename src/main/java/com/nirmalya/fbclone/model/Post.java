4063fa148e732f57498039750010d259f6c4009e
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-03 09:32:42 +0530

    added model

--- src/main/java/com/nirmalya/fbclone/model/Post.java
+++ src/main/java/com/nirmalya/fbclone/model/Post.java
@@ -1,33 +1,22 @@
-package com.nirmalya.fbclone.entity;
+package com.nirmalya.fbclone.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.*;
-
-@Entity
-@Table(name = "posts")
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class PostEntity {
+public class Post {
 
-    @Id
-    @GeneratedValue(generator = "uuid")
-    @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
-
-    @Lob
     private String post;
     private String name;
     private String email;
-
-    @Lob
     private String image;
     private String profilePic;
     private String timeStamp;
+    private String file;
 }
