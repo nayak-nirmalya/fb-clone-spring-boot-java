c00d8e81ec29ca9e8b23de5afac77ba982bc1bff
Nirmalya Nayak <n.nayak263@gmail.com>
2023-03-03 09:12:28 +0530

    defined PostEntityRepository

--- src/main/java/com/nirmalya/fbclone/repository/PostEntityRepository.java
+++ src/main/java/com/nirmalya/fbclone/repository/PostEntityRepository.java
@@ -1,4 +1,9 @@
 package com.nirmalya.fbclone.repository;
 
-public interface PostEntityRepository {
+import com.nirmalya.fbclone.entity.PostEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostEntityRepository extends JpaRepository<PostEntity, String> {
 }
